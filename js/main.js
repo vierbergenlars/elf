9f1eb95b0595d33f6635f8261b29df3811ba4f68
Lars Vierbergen <vierbergenlars@gmail.com>
2014-04-06 16:52:31 +0200

    Improve navigation on mobile; Migrate to LESS
    Add bootstrap animations and JS collapse plugin for better mobile
    navigation.
    Move from plain CSS to LESS stylesheets.
    Add jQuery.scrollTo plugin to scroll smoothly to each section.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
+$('a').on('click', function(event) {
+	var href = $(this).attr('href');
+	if(href[0] != '#') { // Not a local link
+		return;
+	} else {
+		$.scrollTo($(href), 800, {onAfter: function() {document.location.hash = href;}});
+		event.preventDefault();
+	}
+	
+});
+
 
