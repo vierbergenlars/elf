403e2152e401f5a2b89d77438a46f556ff2aed46
Lars Vierbergen <vierbergenlars@gmail.com>
2014-04-14 15:57:51 +0200

    Add facebook page link

--- generator/defaults.php
+++ generator/defaults.php
@@ -3,7 +3,7 @@
 return array(
 		'meta'=> array(
 				'fb_admins'=>'100003609170934',
-				'fb_account'=>'',
+				'fb_account'=>'elfpraesidium',
 				'twitter_account'=>'elf_1415',
 				'root_url'=>'http://vierbergenlars.github.io/turbo-octo-sansa/',
 		),
