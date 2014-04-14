1b31c0be078587c6ad33b37f6340a4e04cddb460
Lars Vierbergen <vierbergenlars@gmail.com>
2014-04-14 21:38:52 +0200

    Add facebook like & share buttons

--- generator/template.php
+++ generator/template.php
@@ -162,10 +162,21 @@
 	<footer>
 		<div class="container">
 			<div class="row">
-				<div class="col-xs-6">&copy; 2014 ELF!</div>
 				<div class="col-xs-6">
-					<p><a href="https://twitter.com/<?=$data['meta']['twitter_account']?>"><i class="fa fa-twitter"></i> @<?=$data['meta']['twitter_account']?></a></p>
-					<p><a href="https://facebook.com/<?=$data['meta']['fb_account']?>"><i class="fa fa-facebook"></i> /<?=$data['meta']['fb_account']?></a></p>
+					<p>&copy; 2014 ELF!</p>
+					<p><div class="fb-like" data-href="https://www.facebook.com/<?=$data['meta']['fb_account']?>" data-layout="standard" data-action="like" data-show-faces="true" data-share="true"></div></p>
+				</div>
+				<div class="col-xs-6">
+					<p>
+						<a
+							href="https://twitter.com/<?=$data['meta']['twitter_account']?>"><i
+							class="fa fa-twitter"></i> @<?=$data['meta']['twitter_account']?>
+						</a>
+					</p>
+					<p>
+						<a href="https://facebook.com/<?=$data['meta']['fb_account']?>"><i
+							class="fa fa-facebook"></i> /<?=$data['meta']['fb_account']?> </a>
+					</p>
 				</div>
 			</div>
 		</div>
@@ -189,5 +200,14 @@
 	  ga('create', 'UA-49996245-1', 'vierbergenlars.github.io');
 	  ga('send', 'pageview');
 	</script>
+	<!-- Facebook SDK -->
+	<div id="fb-root"></div>
+	<script>(function(d, s, id) {
+	  var js, fjs = d.getElementsByTagName(s)[0];
+	  if (d.getElementById(id)) return;
+	  js = d.createElement(s); js.id = id;
+	  js.src = "//connect.facebook.net/en_GB/all.js#xfbml=1";
+	  fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));</script>
 </body>
 </html>
