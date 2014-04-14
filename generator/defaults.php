4e1c02c127a162edbf961a78bd052e2f9a86ba56
Lars Vierbergen <vierbergenlars@gmail.com>
2014-04-14 14:54:48 +0200

    Revert "Fix metadata image urls"
    Forgot to upload the image :trollface:
    This reverts commit 667fd40d7261e4fa442d854acadafcab5c7c76f9.

--- generator/defaults.php
+++ generator/defaults.php
@@ -4,6 +4,5 @@ return array(
 		'meta'=> array(
 				'fb_admins'=>'100003609170934',
 				'twitter_account'=>'vierbergenlars',
-				'root_url'=>'http://vierbergenlars.github.io/turbo-octo-sansa/',
 		),
 );
