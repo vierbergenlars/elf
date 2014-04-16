44a183d80df5fc37582468c1174ce3a3beec0993
Lars Vierbergen <vierbergenlars@gmail.com>
2014-04-16 22:39:36 +0200

    Move pages to a saner URL

--- generator/defaults.php
+++ generator/defaults.php
@@ -5,6 +5,6 @@ return array(
 				'fb_admins'=>'100003609170934',
 				'fb_account'=>'elfpraesidium',
 				'twitter_account'=>'elf_1415',
-				'root_url'=>'http://vierbergenlars.github.io/turbo-octo-sansa/',
+				'root_url'=>'http://vierbergenlars.github.io/elf/',
 		),
 );
