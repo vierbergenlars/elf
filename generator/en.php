0c59e727549dc702b8e0b00665918779784a2d99
Lars Vierbergen <vierbergenlars@gmail.com>
2014-04-18 18:05:15 +0200

    Add english version

--- generator/en.php
+++ generator/en.php
@@ -1,3 +1,197 @@
 <?php
 
-return array();
+return array(
+		'teams'=> array(
+				'praeses'=> array(
+						'title'=>'Praeses',
+						'names'=> 'Jochem',
+						'text'=><<<EOL
+<p>Dear Industrians!</p>
+<p>My name's Jochem Jeunen and I am running for president of Industria next year. This year I worked as vice-president in the shadow of the president and now it's time to do my own thing together with my team and assure you guys of an awesome year!</p>
+<p>How will we do that? Together with my vice-president, we'll be motivating our team and make sure that everyone's helping out. That's how we'll provide you with some of the best activities in Leuven! On the other hand, important contacts with LOKO, Student Council, Group T, ... can't be neglected and that will also be my responsability.</p>
+<p>Of course I'm not doing this alone and I am proud to lead our team together with Sam. Some experienced Industrians, to assure a certain continuity, combined with youthful enthusiasm of our newcomers guarantee an awesome year! I am convinced that E.L.F! is ready to provide you, the students, with a year you will remember for the rest of your student career.</p>
+<p>Have fun!<br>
+Jochem</p>
+EOL
+				),
+				'vice'=> array(
+						'title'=>'Vice-Praeses',
+						'names'=>'Sam',
+						'text'=><<<EOL
+
+EOL
+				),
+				'q'=> array(
+						'title'=>'Quaestor',
+						'names'=>'Stef &amp; Anke',
+						'text'=><<<EOL
+<p>Dear beloved Industrians!</p>
+
+<p>We&rsquo;re Anke and Stef and we&rsquo;ll like to be next year&rsquo;s quaestors (or financial managers)! Whereas Stef has some Industria-experience as an editor & quaestor during the current year, Anke is the newbie who&rsquo;s ready to take matter into her own hands with tons of enthousiasm. Toghether we&rsquo;ll try to run the financial side of our favourite student union.</p>
+<p>We&rsquo;ll make sure that the necessary funding will go to (international-)cantusses, parties (maybe in a new D&eacute;li?) and other activities without ruining our cash-flow. No magic here but some solid money-management!</p>
+<p>This way, we&rsquo;ll attempt to make next year an even better one with more freaky experiences and stories to tell your great-grandchilderen!</p>
+<p>See you guys during the election week,<br>
+Anke &amp; Stef</p>
+EOL
+				),
+				'aa'=>array(
+						'title'=>'Ab-Actis',
+						'names'=>'Maxim',
+						'text'=><<<EOL
+
+EOL
+				),
+				'ontspanning'=>array(
+						'title'=>'Entertainment',
+						'names'=>'Michiel &amp; Ruben',
+						'text'=><<<EOL
+<p>Hello everyone,</p>
+<p>We are Michiel and Ruben, we are the candidates for entertainment as function for Industria. At this moment, there is no fakbar where we can keep our parties as our legendary &lsquo;Proffentap&rsquo;, &lsquo;Beurscaf&eacute;&rsquo;, &lsquo;Duvel Night&rsquo;,&hellip; But next year our fakbar might be back and then these parties and much more can take place. And what else, what keeps us from partying somewhere else?</p>
+<p>Therefore, we will organize next year the necessary cantusses and parties, so you can talk, sing, dance and drink as much as you want. We feel that that&rsquo;s not enough for you, so we will occasionally offer you something to eat and drink in front of Group T. You don&rsquo;t have to move too far to have a nice relaxation between two classes or after class. In short, we want to ensure you that there will be enough entertainment and relaxation.</p>
+<p>Remember: recreation is less boring than effort!</p>
+<p>Cheers!<br>
+Michiel &amp; Ruben</p>
+EOL
+				),
+				'sm'=> array(
+						'title'=>'Master of freshmen',
+						'names'=> 'Fulco &amp; Dries',
+						'text'=><<<EOL
+<p>Commiltones!</p>
+<p>For a fair few years now the amount of freshmen has been steadily increasing and to ensure the follow up with the freshmen is continued we (Dries and Fulco) will be taking on the role of Schachtenmeester together next year.
+We both have been wandering around on Group T for quite some time now and we have the necessary experience to guide them and to get them involved with Industria.</p>
+
+<p>We would like the close the gap a bit between the freshmen and Industria to ensure that the future of Industria remains safe.
+We will attempt to do this by always being accessible for questions and to get as many freshmen to activities of Industria and being there ourselves aswell.
+Of course we will also make sure the traditional activities like freshmen weekend, baptizing, &hellip; will be perfect once more next year.</p>
+
+<p>Give the freshmen what they deserve next year. Vote E.L.F.!</p>
+
+<p>Peace out<br>
+Fulco and Dries</p>
+
+EOL
+				),
+				'kd'=> array(
+						'title'=>'Course service',
+						'names'=>'Pieter &amp; Lotte',
+						'text'=><<<EOL
+<p>Hey future engineers!</p>
+
+<p>Besides the partying and enjoying your student life, you still have to study a little bit to get that highly wanted diploma. For this you need all the right books and that will be our, Pieter Van Petegem and Lotte Verschuere, responsibility next year: The KD.</p>
+
+<p>We have a few goals we want to realize during the year. First we will try to convince the professors to publish their books by us, so you can buy them all at once.</p>
+<p>A next problem we would like to solve is the queue: we want to make the queue shorter, or at least we hope to make your time waiting a bit more enjoyable!</p>
+
+<p>Greetz<br>
+Lotte & Pieter</p>
+EOL
+				),
+				'pc'=>array(
+						'title'=>'PC',
+						'names'=>'Lars',
+						'text'=><<<EOL
+<p>System.out.println("Hello everyone!\\n");</p>
+
+<p>I am Lars Vierbergen, and I will be taking the role of PC next year.</p>
+<p>New with Industria, but with a profound experience in programming and server maintenance.</p>
+<p>Firstly, the development on the brand new site that was promised last year will be continued.
+<br>Moreover, a new KD system is planned, to make the ordering, and more importantly the picking up of books to go more smoothly.
+<br>Of course, the industriLAN party will also be organised next year.</p>
+
+<p>System.exit(0); // Lars</p>
+EOL
+				),
+				'br'=> array(
+						'title'=>'BR',
+						'names'=>'Laurens &amp; Bart',
+						'text'=><<<EOL
+EOL
+				)
+		),
+		'prog'=>array(
+				array(
+						'title'=>'Don&rsquo;t get blue',
+						'text'=><<<EOL
+<p>We&rsquo;re KU Leuven now. That&rsquo;s only the beginning.</p>
+<p>It&rsquo;s a lot harder to organize and finance activities at Group T or somewhere else. That&rsquo;s why it&rsquo;s very important for Industria to work together with Group T and the Student Council to make a great year of the upcoming year.</p>
+<p>How? If we have a good communication with Group T and the Student Council and don&rsquo;t neglect that we can prevent lots of problems.</p>
+<p>And of course we still have to let our voices hear at the LOKO AV.</p>
+EOL
+				),
+				array(
+						'title'=>'Course Service',
+						'text'=> <<<EOL
+<p>We&rsquo;re in Leuven for our studies. The Course Service is the most important function to provide the students with books at the right time.</p>
+<p>We want to provide you with the right books at the right time so you can follow all your courses without any problem.</p>
+EOL
+				),
+				array(
+						'title'=>'Members',
+						'text'=><<<EOL
+<p>Industria is a <em>student</em> organization. Meaning, doing activities for <em>you</em>, guys! We want <em>you</em> to enjoy some entertainment, some sport activities or cultural trips.</p>
+<p>Still know some fun activities? Do you have ideas what every student wants to do? Feel free to let us know!</p>
+<p>But don&rsquo;t hesitate to help us if you want! Do you want to join the best group of builders at the 24h run? No problem! Do you want to organize a fun activity with us? No problem! It&rsquo;s all a possibility!</p>
+
+<p>We want to involve you in our organisation and plan future activities based on your opinion!</p>
+EOL
+				),
+				array(
+						'title'=>'Business Relations',
+						'text'=><<<EOL
+<p>In these financial uncertain times it&rsquo;s important that there enough funds to support the organisation
+so we can organize activities at the lowest prizes. But business relations is also important for the
+students to get a first contact with companies. We want to offer you workshops or interesting
+readings from people with experience. Those are only two examples of things BR can provide you.</p>
+EOL
+				),
+				array(
+						'title'=>'Future',
+						'text'=><<<EOL
+<p>What&rsquo;s going to happen with Industria in the future? What about our &lsquo;fakbar&rsquo; D&eacute;lib&eacute;r&eacute;? What about
+the relation Industria-Student Council? How will life be at Group T within 10 years? Next year will
+be a very important year, to lay a foundation for our Industria successors and to assure they can still
+organize activities as Industria.</p>
+EOL
+				),
+		),
+		'act'=> array(
+				array(
+						'title'=>'ELF! @ Group T',
+						'location'=>'50.874799,4.708097',
+						'location_text'=>'Group T',
+						'fb_id'=>'825123334184359',
+						'start'=>new DateTime('2014-04-22T12:00'),
+						'end'=>new DateTime('2014-04-22T18:00'),
+						'desc'=><<<EOL
+<p>The Eastern holiday has ended and that can only mean one thing! The elections week is coming!</p>
+<p>Before we start the real elections week we&rsquo;re going to let you enjoy some more. Team ELF! provides bratwurst sandwiches and beer at democratic prices.</p>
+<p>See you guys Tuesday!</p>
+EOL
+				),
+		),
+		'weekday' => array(
+				'Su',
+				'Mo',
+				'Tu',
+				'We',
+				'Th',
+				'Fr',
+				'Sa'
+		),
+		'title'=> array(
+				'team'=>'Team',
+				'prog'=>'Goals',
+				'game'=>'2048',
+				'act'=>'Activities',
+		),
+		'game'=> array(
+				'play_2048'=>'Play ELF-2048 now!',
+		),
+		'languages'=> array(
+				'Nederlands'=>'index.html',
+		),
+		'meta' => array(
+				'description'=>'We are ELF!, the Engineer Liberation Front! We are running for the upcoming praesidium elections of Industria \'14-\'15. Vote ELF!',
+		),
+);
