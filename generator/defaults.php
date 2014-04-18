b305f6a2c8c0f6dc53beb0cbfe692d078f223d99
Lars Vierbergen <vierbergenlars@gmail.com>
2014-04-18 21:10:07 +0200

    Update domain pointers

--- generator/defaults.php
+++ generator/defaults.php
@@ -5,6 +5,6 @@ return array(
 				'fb_admins'=>'100003609170934',
 				'fb_account'=>'elfpraesidium',
 				'twitter_account'=>'elf_1415',
-				'root_url'=>'http://vierbergenlars.github.io/elf/',
+				'root_url'=>'http://elf-praesidium.be/',
 		),
 );
